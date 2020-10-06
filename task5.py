05b2bb3be6f03f7c63c69ebdcac44eb238bc273b
Jeremy Smith <jeremysmith@Jeremys-MacBook-Pro.local>
2020-10-07 01:52:12 +1100

    solution for task-6

--- task5.py
+++ task5.py
@@ -3,3 +3,5 @@ if (a%2) == 1:
     print("Odd")
 else:
     print("Even")
+
+
---
+++ task6.py
@@ -0,0 +1,15 @@
+
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 undefined <undefined>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+for x in range(1,11):
+    if (x)%2 == 0:
+        print(x)
